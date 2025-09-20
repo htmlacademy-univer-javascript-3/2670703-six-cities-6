70053fb8c7169b0f647f39edb03d32da4b103aed
Keks <keks@htmlacademy.ru>
2025-09-20 10:52:25 +0300

    :cat:

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,9 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'markup',
-  publicDir: '../public',
-  build: { outDir: '../dist' },
-  resolve: {
-    alias: {
-      '/src': path.resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    fs: {
-      allow: [path.resolve(__dirname, '.')],
-    },
-  },
   plugins: [react()],
   test: {
     globals: true,
