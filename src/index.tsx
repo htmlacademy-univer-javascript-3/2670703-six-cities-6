38dd5d14405bf90a9154385346c8d062fe3a43d3
icerzack <maxalkuz@gmail.com>
2025-12-16 15:39:42 +0700

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const OFFERS_COUNT = 312;
+
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App offersCount={OFFERS_COUNT} />
   </React.StrictMode>
 );
