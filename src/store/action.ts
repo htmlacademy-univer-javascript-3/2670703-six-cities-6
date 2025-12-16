ef7ddd017fc151816e57fe48e142ec97fd3ca193
icerzack <maxalkuz@gmail.com>
2025-12-16 21:54:34 +0700

    commit

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,6 +1,11 @@
 import { createAction } from '@reduxjs/toolkit';
 import type { Offer } from '../mocks/offers';
+import { SortingType } from '../const';
 
 export const changeCity = createAction<string>('offers/changeCity');
 
 export const loadOffers = createAction<Offer[]>('offers/loadOffers');
+
+export const changeSortingType = createAction<SortingType>('offers/changeSortingType');
+
+export const setHoveredOfferId = createAction<number | null>('offers/setHoveredOfferId');
